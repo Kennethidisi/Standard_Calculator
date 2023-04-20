8b5982fdd1ca3a5c8a497f7f0ccf64e89193209a
Kennethidisi <127211509+Kennethidisi@users.noreply.github.com>
2023-04-20 17:50:27 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let input_display = document.querySelector('#input_display')
+
+window.onload = ()=>{
+    input_display.value = ""
+}
+
+let clear_btn = document.querySelector('#clear');
+let delete_btn = document.querySelector('#delete');
+let equal_to = document.querySelector('#equal_to');
+
+let input_btns = document.querySelectorAll('.input_btn');
+input_btns.forEach((btn)=>{
+    btn.addEventListener('click', ()=>{
+        input_display.value += btn.value
+    })
+})
+
+equal_to.addEventListener('click', ()=>{
+    try{
+        let result = eval(input_display.value)
+
+        if(Number.isInteger(result)){
+            input_display.value = result;
+        }else{
+            input_display.value = result.toFixed(2);
+        }
+    }catch(err){
+        console.log(err)
+        input_display.value = 'MATHEMATICAL ERROR'
+        setTimeout(()=>{
+            input_display.value = ''
+        },2000)
+    }
+})
+
+
+clear_btn.addEventListener('click', ()=>{
+    input_display.value = "";
+})
+
+delete_btn.addEventListener('click', ()=>{
+    input_display.value = input_display.value.substring(0, input_display.value.length-1)
+})
